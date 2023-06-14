4088c66253d042cb51c9ff4741ceb142fa1027b4
sumanpandit <sumanpanditofficial@gmail.com>
2023-06-14 21:45:34 +0545

    Incomplete simon game and some jquery starting code

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,9 @@
 //This is the example of jquery.
-$("h1").css("color", "red");
+$("h1").css("font-size", "5rem");
+$("button").click(function () {
+    $("h1").css("color", "green");
+});
+
+$("input").keydown(function (event) {
+    $("h1").text(event.key);
+});
