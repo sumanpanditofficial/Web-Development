53b8f9ba84ac8b7c6f8687c7c9d18c848107c0d2
sumanpandit <sumanpanditofficial@gmail.com>
2023-06-14 21:47:43 +0545

    Incomplete simon game

---
+++ Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,16 @@
+$("#green").click(function () {
+    var playAudio = new Audio("sounds/green.mp3");
+    playAudio.play();
+});
+$("#red").click(function () {
+    var playAudio = new Audio("sounds/red.mp3");
+    playAudio.play();
+});
+$("#yellow").click(function () {
+    var playAudio = new Audio("sounds/yellow.mp3");
+    playAudio.play();
+});
+$("#blue").click(function () {
+    var playAudio = new Audio("sounds/blue.mp3");
+    playAudio.play();
+});
